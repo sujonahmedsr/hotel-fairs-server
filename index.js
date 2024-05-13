0058be200d726c65208ff25c7c45f9a8c7418032
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-05-13 10:46:22 +0600

    all data get perfectly

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 
 // midlleware 
-app.use(cors())
+app.use(cors({
+  origin:[
+    'http://localhost:5173'
+  ],
+  credentials: true,
+}))
 app.use(express.json())
 
 
@@ -29,10 +34,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const roomsCollection = client.db('OurRooms').collection('rooms')
+
+    app.get('/rooms', async(req, res)=>{
+        const cursor = roomsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/rooms/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)}
+        const result = await roomsCollection.findOne(query)
+        res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
